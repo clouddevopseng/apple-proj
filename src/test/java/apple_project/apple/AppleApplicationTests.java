a11364cb966b5f2c6aa1f4df2cc6f9890b0451f7
venkat <venkat@tcs.com>
2025-03-17 09:13:02 +0530

    apple-proj

---
+++ src/test/java/apple_project/apple/AppleApplicationTests.java
@@ -0,0 +1,13 @@
+package apple_project.apple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
